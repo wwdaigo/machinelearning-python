12bd7552725d93b28b07693d636598fbb2b99ada
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-05-06 00:55:39 -0300

    NP test functions

---
+++ np-functions.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.zeros((2,2))
+print a
+
+b = np.ones((1,2))
+print b
+
+c = np.full((2,2), 7)
+print c
+
+d = np.eye(2) # identity matrix 2x2
+print d
+
+e = np.random.random((2,2))
+print e
